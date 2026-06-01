48b159268e15b8517b733a670d94d162d656ca04
mariummun <munnimarium5@gmail.com>
2026-06-01 23:25:45 +0600

    added a new object

--- first.js
+++ first.js
@@ -1,18 +1,25 @@
-{
-    let a = 5;
-    console.log(a);
-}
+// {
+//     let a = 5;
+//     console.log(a);
+// }
 
-{
-    let a = 15;
-    console.log(a);
-}
+// {
+//     let a = 15;
+//     console.log(a);
+// }
 
-const student = {
-		fullName: "ABCD",
-		isPass: true,
-		marks: 80,
-        age: 20
+// const student = {
+// 		fullName: "ABCD",
+// 		isPass: true,
+// 		marks: 80,
+//         age: 20
+// };
+// console.log(student.age);
+// console.log(student["age"]);
+
+const profile = {
+    username : "marium",
+    followerCount : 10,
+    followingCount : 150,
+    isFollow : true
 };
-console.log(student.age);
-console.log(student["age"]);
