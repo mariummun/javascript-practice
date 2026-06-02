69e7e03d5a41786d31e59eebc6d63489ae8eae60
mariummun <munnimarium5@gmail.com>
2026-06-03 00:02:43 +0600

    learning prompt

--- first.js
+++ first.js
@@ -17,21 +17,35 @@
 // console.log(student.age);
 // console.log(student["age"]);
 
-const profile = {
-    username : "marium",
-    followerCount : 10,
-    followingCount : 150,
-    isFollow : true
-};
+// const profile = {
+//     username : "marium",
+//     followerCount : 10,
+//     followingCount : 150,
+//     isFollow : true
+// };
+// {
+//     let value = 4;
+//     if (value % 2 === 0){
+//         console.log("even");
+//     }
+//     else{
+//         console.log("odd");
+//     }
+// }
 
-let value = 4;
-if (value % 2 === 0){
-    console.log("even");
-}
-else{
-    console.log("odd");
-}
+// {
+//     let age = 48;
+//     let result = age > 18 ? "an adult" : "not an adult";
+//     console.log(result);
+// }
 
-let age = 48;
-let result = age > 18 ? "an adult" : "not an adult";
-console.log(result);
+
+{
+    let number = prompt("enter a number: ");
+    if(number % 5 == 0){
+        console.log("divisible by 5");
+    }
+    else{
+        console.log("not divisible by 5");
+    }
+}
